29c6673195ba35f4aaef74617db8138b08bcacae
Daniel otowski <danielotowski@googlemail.com>
2024-02-03 20:47:44 +0000

    Imstall react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import  Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World! this is a great view!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
